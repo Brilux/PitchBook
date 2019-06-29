4122050e7a820d7cfa19bfff9532e23837edb374
brilux <brilux96@gmail.com>
2019-06-29 03:49:41 +0300

    update tab switch and add container for two first sections

--- js/tab-switch.js
+++ js/tab-switch.js
@@ -1,28 +1,43 @@
-document.onkeydown = function() {
-  if (event.keyCode === 9) {
-    const firstTab = document.getElementById('firstTab');
-    const secondTab = document.getElementById('secondTab');
-    const thirdTab = document.getElementById('thirdTab');
+const firstTab = document.getElementById('firstTab');
+const secondTab = document.getElementById('secondTab');
+const thirdTab = document.getElementById('thirdTab');
 
-    const firstFeatureTab = document.getElementById('firstFeatureTab');
-    const secondFeatureTab = document.getElementById('secondFeatureTab');
-    const thirdFeatureTab = document.getElementById('thirdFeatureTab');
+const firstFeatureTab = document.getElementById('firstFeatureTab');
+const secondFeatureTab = document.getElementById('secondFeatureTab');
+const thirdFeatureTab = document.getElementById('thirdFeatureTab');
 
-    if (firstTab.className === 'features-tab-orange') {
-      firstTab.className = 'features-tab';
-      firstFeatureTab.className = 'feature-unactive-container';
-      secondTab.className = 'features-tab-orange';
-      secondFeatureTab.className = 'feature-active-container';
-    } else if (secondTab.className === 'features-tab-orange') {
-      secondTab.className = 'features-tab';
-      secondFeatureTab.className = 'feature-unactive-container';
-      thirdTab.className = 'features-tab-orange';
-      thirdFeatureTab.className = 'feature-active-container';
-    } else {
-      thirdTab.className = 'features-tab';
-      thirdFeatureTab.className = 'feature-unactive-container';
-      firstTab.className = 'features-tab-orange';
-      firstFeatureTab.className = 'feature-active-container';
-    }
-  }
-};
+function resetFirstTab() {
+  firstTab.className = 'features-tab';
+  firstFeatureTab.className = 'feature-unactive-container';
+}
+
+function resetSecondTab() {
+  secondTab.className = 'features-tab';
+  secondFeatureTab.className = 'feature-unactive-container';
+}
+
+function resetThirdTab() {
+  thirdTab.className = 'features-tab';
+  thirdFeatureTab.className = 'feature-unactive-container';
+}
+
+function switchFirstTab() {
+  resetSecondTab();
+  resetThirdTab();
+  firstTab.className = 'features-tab-orange';
+  firstFeatureTab.className = 'feature-active-container';
+}
+
+function switchSecondTab() {
+  resetFirstTab();
+  resetThirdTab();
+  secondTab.className = 'features-tab-orange';
+  secondFeatureTab.className = 'feature-active-container';
+}
+
+function switchThirdTab() {
+  resetFirstTab();
+  resetSecondTab();
+  thirdTab.className = 'features-tab-orange';
+  thirdFeatureTab.className = 'feature-active-container';
+}
