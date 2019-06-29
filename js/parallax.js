130f7bc3613763f88f1b46d77db76267aa532a35
brilux <brilux96@gmail.com>
2019-06-30 00:04:52 +0300

    update parallax

--- js/parallax.js
+++
@@ -1,7 +0,0 @@
-function parallax(event) {
-    this.querySelectorAll('.axure-design-title-container').forEach(section => {
-      section.style.transform = `translateX(${event.clientX/30}px`;
-    });
-}
-
-document.addEventListener('mousemove', parallax);
