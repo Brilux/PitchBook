5d702deaff4884218b8973b8486ba2ee072d7396
brilux <brilux96@gmail.com>
2019-07-15 21:16:45 +0300

    update tab switch

--- js/tab-switch.js
+++ js/tab-switch.js
@@ -1,43 +1,15 @@
-const firstTab = document.getElementById('firstTab');
-const secondTab = document.getElementById('secondTab');
-const thirdTab = document.getElementById('thirdTab');
-
-const firstFeatureTab = document.getElementById('firstFeatureTab');
-const secondFeatureTab = document.getElementById('secondFeatureTab');
-const thirdFeatureTab = document.getElementById('thirdFeatureTab');
-
-let resetFirstTab = () => {
-  firstTab.className = 'features-tab';
-  firstFeatureTab.className = 'feature-unactive-container';
-};
-
-let resetSecondTab = () => {
-  secondTab.className = 'features-tab';
-  secondFeatureTab.className = 'feature-unactive-container';
-};
-
-let resetThirdTab = () => {
-  thirdTab.className = 'features-tab';
-  thirdFeatureTab.className = 'feature-unactive-container';
-};
-
-let switchFirstTab = () => {
-  resetSecondTab();
-  resetThirdTab();
-  firstTab.className = 'features-tab-orange';
-  firstFeatureTab.className = 'feature-active-container';
-};
-
-let switchSecondTab = ()  => {
-  resetFirstTab();
-  resetThirdTab();
-  secondTab.className = 'features-tab-orange';
-  secondFeatureTab.className = 'feature-active-container';
-};
-
-let switchThirdTab = () => {
-  resetFirstTab();
-  resetSecondTab();
-  thirdTab.className = 'features-tab-orange';
-  thirdFeatureTab.className = 'feature-active-container';
-};
+(function ($) {
+  $(function () {
+    $('ul.tabs-container').on('click', 'li:not(.features-tab.orange)', function () {
+      $(this)
+          .addClass('orange')
+          .siblings()
+          .removeClass('orange')
+          .closest('div.features-content-container')
+          .find('div.feature-container')
+          .removeClass('active')
+          .eq($(this).index())
+          .addClass('active');
+    });
+  });
+})(jQuery);
